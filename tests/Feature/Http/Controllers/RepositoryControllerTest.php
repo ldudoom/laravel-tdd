2fc82c249a5dbbd09a30115edb1f02fb41592683
Raul Chauvin <raullduch@gmail.com>
2022-01-06 18:44:46 -0500

    Completo la prueba validando todas las rutas del recurso y verificando que en todos los casos se realice una redireccion al login

--- tests/Feature/Http/Controllers/RepositoryControllerTest.php
+++ tests/Feature/Http/Controllers/RepositoryControllerTest.php
@@ -15,6 +15,12 @@ class RepositoryControllerTest extends TestCase
      */
     public function test_guest()
     {
-        $this->get('repositories')->assertRedirect('login');
+        $this->get('/repositories')->assertRedirect('login');           // index
+        $this->get('/repositories/create')->assertRedirect('login');    // Create form
+        $this->post('/repositories', [])->assertRedirect('login');      // Store
+        $this->get('/repositories/1')->assertRedirect('login');         // Show
+        $this->get('/repositories/1/edit')->assertRedirect('login');    // Edit
+        $this->patch('/repositories/1')->assertRedirect('login');       // Update
+        $this->delete('/repositories/1')->assertRedirect('login');      // Destroy
     }
 }
