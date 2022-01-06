54296f567643aa59b0f19dbe3295d969a41ade62
Raul Chauvin <raullduch@gmail.com>
2022-01-06 17:07:45 -0500

    Agregado comentarios al codigo fuente

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -11,8 +11,10 @@ class UserTest extends TestCase
 
     public function test_has_many_repositories()
     {
+        // Creamos una nueva instancia de User
         $oUser = new User;
-
+        // Verificamos que la relacion este correcta, verificando que el metodo repositories de la clase User genere una
+        // coleccion de la clase Repository
         $this->assertInstanceOf(Collection::class, $oUser->repositories);
     }
 }
