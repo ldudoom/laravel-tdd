d7ab0654d8a69c74c3851d27a2cb8624a4f786d0
Raul Chauvin <raullduch@gmail.com>
2022-01-07 18:49:54 -0500

    Agregamos la validacion en el metodo update del controlador para que unicamente un usuario pueda editar los repositorios creados por el

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Repository;
 
+use Illuminate\Support\Facades\Auth;
+
 class RepositoryController extends Controller
 {
 
@@ -34,11 +36,25 @@ class RepositoryController extends Controller
             'description' => 'required'
         ]);
 
+        // Verificamos si el usuario logueado esta intentando actualizar un repositorio que no le pertenece
+        // en cuyo caso devolvemos un estado HTTP 403
+        if(Auth::user()->id != $repository->user_id){
+            abort(403);
+        }
+
         // Debido a que ya tenemos un usuario logueado, podemos utilizarlo para crear un repositorio
         // que le pertenezca
         $repository->update($request->all());
 
         // Ahora, una vez que el repositorio esta creado, procedemos a realizar la redireccion
         return redirect()->route('repositories.edit', $repository);
+    }
+
+    public function destroy(Repository $repository){
+
+        // Eliminamos el repositorio de la base de datos
+        $repository->delete();
+        // Redirigimos al usuario a la pantalla de lista de repositorios
+        return redirect()->route('repositories.index');
     }
 }
