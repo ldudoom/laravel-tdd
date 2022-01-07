0d0daf02fa88943c84c3bf3889bc5725cd305a9f
Raul Chauvin <raullduch@gmail.com>
2022-01-07 17:43:08 -0500

    Agregando funcionalidad al controlador para que el sistema permita actualizar un repositorio y que valide que no se puedan crear ni actualizar repositorios cuando el request llegue en blanco

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -4,16 +4,41 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+use App\Models\Repository;
+
 class RepositoryController extends Controller
 {
 
 
     public function store(Request $request){
+
+        $request->validate([
+            'url' => 'required|url',
+            'description' => 'required'
+        ]);
+
         // Debido a que ya tenemos un usuario logueado, podemos utilizarlo para crear un repositorio
         // que le pertenezca
         $request->user()->repositories()->create($request->all());
 
         // Ahora, una vez que el repositorio esta creado, procedemos a realizar la redireccion
         return redirect()->route('repositories.index');
+    }
+
+
+    public function update(Request $request, Repository $repository){
+
+        $request->validate([
+            'url' => 'required|url',
+            'description' => 'required'
+        ]);
+
+        // Debido a que ya tenemos un usuario logueado, podemos utilizarlo para crear un repositorio
+        // que le pertenezca
+        $repository->update($request->all());
+
+        // Ahora, una vez que el repositorio esta creado, procedemos a realizar la redireccion
+        return redirect()->route('repositories.edit', $repository);
     }
 }
