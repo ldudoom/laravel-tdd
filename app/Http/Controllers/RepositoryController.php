8c128ffdb9edcc254376a9c1bcab75e895da0c09
Raul Chauvin <raul.chauvin@telefonica.com>
2022-01-07 12:32:54 -0500

    Implementamos el metodo store en el controlador para almacenar un registro de forma masiva en la tabla repositories y luego hacemos una redireccion a la lista de repositorios

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class RepositoryController extends Controller
 {
-    //
+
+
+    public function store(Request $request){
+        // Debido a que ya tenemos un usuario logueado, podemos utilizarlo para crear un repositorio
+        // que le pertenezca
+        $request->user()->repositories()->create($request->all());
+
+        // Ahora, una vez que el repositorio esta creado, procedemos a realizar la redireccion
+        return redirect()->route('repositories.index');
+    }
 }
