59f9e595fec60fbc16e637ffd6afc96cc14ab762
Raul Chauvin <raul.chauvin@telefonica.com>
2022-01-14 00:18:36 -0500

    Agregamos una politica para refactorizar y optimizar nuestro codigo, esta politica va a validar que el usuario sea el propietario de un repositorio para poder administrarlo

---
+++ app/Policies/RepositoryPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Repository;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class RepositoryPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+
+    public function pass(User $oUser, Repository $oRepository){
+        return $oUser->id == $oRepository->user_id;
+    }
+}
