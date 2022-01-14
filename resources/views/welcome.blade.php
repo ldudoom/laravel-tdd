247259a048eff3498409e52dd5cf688925119700
Raul Chauvin <raul.chauvin@telefonica.com>
2022-01-13 23:27:57 -0500

    Agregando clases tailwind a la pagina home del proyecto para mejorar su apariencia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,32 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
     </head>
     <body class="bg-gray-200">
-        @foreach ($aRepositories as $oRepository)
-            <h2>{{ $oRepository->url }}</h2>
-            <p>{{ $oRepository->description }}</p>
-        @endforeach
+        <ul class="max-w-lg bg-white border-r border-gray-300 shadow-xl">
+            @foreach ($aRepositories as $oRepository)
+                <li class="flex items-center text-black p-2 hover:bg-gray-300">
+                    <img
+                        src="{{ $oRepository->user->profile_photo_url }}"
+                        class="w-12 h-12 rounded-full mr-2"
+                    >
+                    <div class="flex justify-between w-full">
+                        <div class="flex-1">
+                            <h2 class="text-sm font-semibold text-black">{{ $oRepository->url }}</h2>
+                            <p>{{ $oRepository->description }}</p>
+                        </div>
+                        <span class="text-xs font-medium text-gray-600">{{ $oRepository->created_at }}</span>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
     </body>
 </html>
