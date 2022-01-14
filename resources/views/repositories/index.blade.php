f7e5b1c764b5e59ef6678073e6b6b966b4b13a43
Raul Chauvin <raul.chauvin@telefonica.com>
2022-01-14 00:27:17 -0500

    Para finalizar el proyecto agregamos los botones de agregar un nuevo repositorio en la vista de lista de repositorios, y el boton de eliminar repositorio para cada uno de los elementos de la lista de repositorios

--- resources/views/repositories/index.blade.php
+++ resources/views/repositories/index.blade.php
@@ -7,12 +7,18 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <p class="text-right mb-4">
+                <a href="{{ route('repositories.create') }}" class="bg-blue-500 text-black font-bold py-2 px-4 rounded-md text-xs">
+                    Add Repository
+                </a>
+            </p>
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
                 <table>
                     <thead>
                         <tr>
                             <th class="border px-4 py-2">ID</th>
                             <th class="border px-4 py-2">ENLACE</th>
+                            <th class="border px-4 py-2">&nbsp;</th>
                             <th class="border px-4 py-2">&nbsp;</th>
                             <th class="border px-4 py-2">&nbsp;</th>
                         </tr>
@@ -28,10 +34,17 @@
                                 <td class="px-4 py-2">
                                     <a href="{{ route('repositories.edit', $oRepository) }}">Editar</a>
                                 </td>
+                                <td class="px-4 py-2">
+                                    <form action="{{ route('repositories.destroy', $oRepository) }}" method="POST">
+                                        @csrf
+                                        @method('delete')
+                                        <input type="submit" value="Eliminar" class="px-4 rounded-md bg-red-500 text-black">
+                                    </form>
+                                </td>
                             </tr>
                         @empty
                             <tr>
-                                <td colspan="4">No tienes repositorios creados</td>
+                                <td colspan="5">No tienes repositorios creados</td>
                             </tr>
                         @endforelse
                     </tbody>
